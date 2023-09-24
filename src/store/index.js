9d12a3dcbb750a6df1514a610c8b3a000b264316
Derrick Strong <derrickmstrong@gmail.com>
2023-09-23 22:42:34 -0500

    Update initial data

--- src/store/index.js
+++ src/store/index.js
@@ -6,19 +6,4 @@ export const FeedbackStore = writable([
         rating: 10,
         text: "Svelte is awesome!"
     },
-    {
-        id: 2,
-        rating: 8,
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, quis aliquam nisl nisl quis nisl."
-    },
-    {
-        id: 3,
-        rating: 3,
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, quis aliquam nisl nisl quis nisl."
-    },
-    {
-        id: 4,
-        rating: 7,
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    }
 ]);
